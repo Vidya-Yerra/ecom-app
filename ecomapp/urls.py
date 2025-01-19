445e8a418eef3b79afa5bffe9e8f02f3207b9005
Vidya <vidyayerra7@gmail.com>
2025-01-19 10:36:15 +0530

    Added review page and view to create new review

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,include
-from . views import authView,home,productsView
+from . views import authView,home,productsView, reviewView
 
 urlpatterns = [
     path("",home,name="home"),
     path('signup/',authView, name="authView"),
     path('accounts/',include("django.contrib.auth.urls")),
-    path('products/',productsView, name="productsview")
+    path('products/',productsView, name="productsview"),
+    path('product/review/<int:product_id>',reviewView,name='reviewview')
+
     
 
 ]
--- ecomapp/views.py
+++ ecomapp/views.py
@@ -6,6 +6,7 @@ from ecomapp.models import Product,Review
 from ecomapp.serializers import ProductSerializer,ReviewSerializer
 from rest_framework import viewsets
 from django.shortcuts import redirect
+import datetime
 
 # Create your views here.
 
@@ -26,6 +27,20 @@ def authView(request):
 def productsView(request):
     products = Product.objects.all()
     return render(request,"products.html",{"products":products})
+
+def reviewView(request,product_id):
+    print("In review view function")
+    product = Product.objects.get(id=product_id)
+    if request.method == 'POST':
+        user=request.user
+        text=request.POST.get('review_product')
+        currentdatetime=datetime.datetime.now()
+        review=Review(user=user,product=product,content=text,datetime=currentdatetime)
+        review.save()
+        return redirect('/ecom/products/')
+    else:
+        return render(request,"review.html",{"product":product})
+
 
 
 class ProductViewSet(viewsets.ModelViewSet):
